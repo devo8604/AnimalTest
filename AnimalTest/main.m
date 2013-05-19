2b65b75e13fef84c79da6c0945b057d05ea43d8d
Devon Smith <devo8604@gmail.com>
2013-05-19 11:20:19 -0800

    Initial Commit

---
+++ AnimalTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  AnimalTest
+//
+//  Created by Devon Smith on 5/19/13.
+//  Copyright (c) 2013 Devon Smith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
